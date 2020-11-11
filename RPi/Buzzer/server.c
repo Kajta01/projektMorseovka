5fd7c5721344763ee616821ae68d61eeb4253350
Kajta01 <37844835+Kajta01@users.noreply.github.com>
2020-11-11 18:32:08 +0100

    funguje

--- RPi/Buzzer/server.c
+++ RPi/Buzzer/server.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,54 +9,53 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 
+int getData(int sockfd) {
+	char buffer[32];
+	int n;
 
-int getData( int sockfd ) {
-  char buffer[32];
-  int n;
-
-  if ( (n = read(sockfd,buffer,31) ) < 0 )
-    error( const_cast<char *>( "ERROR reading from socket") );
-  buffer[n] = '\0';
-  return atoi( buffer );
+	if ((n = read(sockfd, buffer, 31)) < 0)
+		printf("ERROR reading from socket");
+	buffer[n] = '\0';
+	return atoi(buffer);
 }
 
+int main(int argc, char *argv[]) {
 
-int main(int argc, char *argv[]){
+	int socketId, newsockfd;
+	int data = 0;
 
-    int socketId, newsockfd;
-    int n = 0;
- 
-    char recvBuff[1024];
-    struct sockaddr_in serv_addr;
+	char recvBuff[1024];
+	struct sockaddr_in serv_addr;
 
-    memset(&recvBuff, '0', sizeof(recvBuff) );
-    memset(serv_addr, '0', sizeof(serv_addr) );
+	memset(recvBuff, '0', sizeof(recvBuff));
+	memset(&serv_addr, '0', sizeof(serv_addr));
 
-    puts("Server");
+	puts("Server");
 
-    socketId = socket(AF_INET,SOCK_STREAM,0);
-    if(socketId < 0) return 1;
+	socketId = socket(AF_INET, SOCK_STREAM, 0);
+	if (socketId < 0)
+		return 1;
 
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(5000);
-    serv_addr.sin_addr.s_addr=htonl(INADDR_ANY);
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(5000);
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-    bind(socketId,(struct sockaddr *) &serv_addr, sizeof(serv_addr));
-    listen(socketInt,10);
+	bind(socketId, (struct sockaddr*) &serv_addr, sizeof(serv_addr));
+	listen(socketId, 10);
 
-    while(1)
-    {
-        printf( "waiting for new client...\n" );
-        if ( ( newsockfd = accept( socketId, (struct sockaddr *) NULL,NULL) ) < 0 )
-            error( const_cast<char *>("ERROR on accept") );
-        printf( "opened new communication with client\n" );
-        while ( 1 ) {
-	     //---- wait for a number from client ---
-             data = getData( newsockfd );
-             printf( "got %d\n", data );
-             if ( data < 0 ) 
-                break;
-    }
-    
-    return EXIT_SUCCESS;
+	while (1) {
+		printf("waiting for new client...\n");
+		if ((newsockfd = accept(socketId, (struct sockaddr*) NULL, NULL)) < 0)
+
+			printf("opened new communication with client\n");
+		while (1) {
+			//---- wait for a number from client ---
+			data = getData(newsockfd);
+			printf("got %d\n", data);
+			if (data < 0)
+				break;
+		}
+	}
+
+	return EXIT_SUCCESS;
 }
