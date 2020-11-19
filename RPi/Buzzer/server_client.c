506cf332aefa73782feb1ac3a51b309687556cd0
Kajta01 <37844835+Kajta01@users.noreply.github.com>
2020-11-19 12:04:29 +0100

    z parametru

--- RPi/Buzzer/server_client.c
+++ RPi/Buzzer/server_client.c
@@ -1,4 +1,3 @@
-//192.168.0.109
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,35 +10,53 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[]){
 
-    int socketId;
-    int n = 0;
-    char recvBuff[1024];
-    struct sockaddr_in serv_addr;
+int main(int argc, char *argv[]) {
 
-    memset(&recvBuff, '0', sizeof(recvBuff) );
-    memset(serv_addr, '0', sizeof(serv_addr) );
+	int socketInt;
+	int n=0;
+	char recvBuff[1024];
+	char sendBuff[1024];
+	struct sockaddr_in serv_addr;
 
-    puts("Client");
+	char *ipAdresa;
+	char *message;
 
-    socketId = socket(AF_INET,SOCK_STREAM,0);
-    if(socketId < 0) return 1;
+	memset(sendBuff, '0', sizeof(sendBuff));
+	memset(recvBuff, '0', sizeof(recvBuff));
+	memset(&serv_addr, '0', sizeof(serv_addr));
 
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(5000);
-    inet_pton(AF_INET,"192.168.0.109" , &serv_addr.sin_addr); //argv[1]
+	if(argc == 1){
+		ipAdresa = "192.168.0.109";
+		message = "usmej se";
+	}
+	else{
+		ipAdresa = argv[1];
+		message = argv[2];
+	}
 
-    if(connect(socketId, (struct sockaddr *) &serv_addr, sizeof(serv_addr))<0)
-    {
-        printf("Nelze se pripojit!");
-        return 1;
-    }
-    while(n = read(socketId, recvBuff, sizeof(recvBuff-1))>0)
-    {
-        recvBuff[n]=0;
-        printf("Přijato %s\n",recvBuff);
-    }
-    
-    return EXIT_SUCCESS;
+	puts("Socket_Client"); /* prints Socket_Client */
+
+	socketInt = socket(AF_INET, SOCK_STREAM, 0); //vytvoření socketu
+
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(5000);	//nastavení portu
+	inet_pton(AF_INET, ipAdresa,&serv_addr.sin_addr); //nastavení IP
+
+	if(connect(socketInt, (struct sockaddr *)&serv_addr, sizeof(serv_addr))<0)
+	{
+		printf("Nelze se pripojit! \n");
+		return 1;
+	}
+
+	//odeslání dat pro server
+	snprintf(sendBuff,sizeof(sendBuff), "%s", message);
+	if(write(socketInt,sendBuff,strlen(sendBuff))<1)
+	{
+		printf("chyba zapisu na server");
+		return 1;
+	}
+
+
+	return EXIT_SUCCESS;
 }
